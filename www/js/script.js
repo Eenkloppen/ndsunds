7fe3d6ad6621fe978d97692069dcc04bf92d17c1
Vlad Vinogradov <Noname@MacbookE.local>
2020-06-11 22:02:47 +0500

    fornt

--- www/js/script.js
+++ www/js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $('#fullpage').fullpage({
         autoScrolling: true,
         css3: true,
-        sectionsColor: ['blue', 'orange'],
+        sectionsColor: ['#003333', ' #006633', '#cc99ff', '#336666'],
         navigation: true,
     });
 
